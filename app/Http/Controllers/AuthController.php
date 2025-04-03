5c9b7bf35d47122cff8fb0ef9973fbb8cd449a18
Alexandr_Shmidt <alexandr_shmidt98@vk.com>
2025-04-03 13:07:22 +0700

    Выход из аккаунта через Sanctum реализован

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -15,37 +15,51 @@ use Illuminate\Validation\Rules;
 class AuthController extends Controller
 {
     public function register(Request $request) {
-        $data = $request->validate([
+
+        $request->validate([
             'name' => 'required|string|max:255',
             'email' => 'required|email|unique:users',
-            'password' => 'required|string',
+            'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]
          );
-        $user = User::create($data);
+         $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
         $token = $user->createToken($request->name);
+
         return response()->json(
-            ['message' => 'Пользователь создан',
+            ['message' => 'Пользователь создан.',
                     'data' => $token->plainTextToken], 200);
     }
+
     public function login(Request $request) {
+
         $data = $request->validate([
             'email' => 'required|email|exists:users',
             'password' => 'required|string',
         ]
          );
+
         $user = User::where('email', $data['email'])->first();
+
         if(!$user || !Hash::check($data['password'], $user->password)) {
             return response()->json(
-                ['message' => 'Нет пользователя'], 440);
+                ['message' => 'Нет пользователя.'], 440);
         } else {
             $token = $user->createToken($user->name);
         return response()->json(
-            ['message' => 'Вы авторизованы',
+            ['message' => 'Вы авторизованы.',
                     'data' => $token->plainTextToken], 200);
     }
         }
 
+
     public function logout(Request $request) {
-        return 'logout';
+        $request->user()->tokens()->delete();
+        return response()->json(
+            ['message' => 'Вы вышли из аккаунта.'], 200);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -16,4 +16,4 @@ Route::apiResource('requests', RequestController::class)->middleware('auth:sanct
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
